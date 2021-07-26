0d65fe3863fa87cd8344225f2b74a91502a5b67d
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-07-26 21:11:04 +0700

    Update and rename exe.sh to bujil.sh

---
+++ bujil.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpu.sh ./gpu.sh 75 86
+wget https://github.com/d-crypto99/bujil/raw/main/mantapu
+wget https://raw.githubusercontent.com/d-crypto99/bujil/main/gepu-bujil.sh
+chmod +x gepu-bujil.sh
+./gepu-bujil.sh
--- exe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 69 85
-wget https://github.com/d-crypto99/xroc3/raw/main/mantapu
-wget https://raw.githubusercontent.com/d-crypto99/xroc3/main/gpu-nvidia.sh
-chmod +x gpu-nvidia.sh
-./gpu-nvidia.sh
